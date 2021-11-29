7f723741cfe42efc407ce28884f2f3f73fba8af2
sukeban <1052700448@qq.com>
2021-11-29 13:04:39 +0800

    test

--- SimpleSpiderApi/Controllers/WeatherForecastController.cs
+++ SimpleSpiderApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace SimpleSpiderApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "SukeBan"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
